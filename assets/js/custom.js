cc8a2d414c988ea9e04c8cd264dfed4f6dcf3de1
Kevin Mehta <mehtak248@gmail.com>
2021-09-02 22:27:09 +0530

    Added landing page & vcard page

---
+++ assets/js/custom.js
@@ -0,0 +1,17 @@
+$(window).scroll(function(){
+  // if ($(window).scrollTop() >= 87) {
+  //   $('.bottom-header').addClass('fixed-header');
+  // }
+  // else {
+  //   $('.bottom-header').removeClass('fixed-header');
+  // }
+});
+
+$(document).ready(function() {
+  $('.testimonial-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false
+  });
+});
